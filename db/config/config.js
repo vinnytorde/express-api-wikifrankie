19fbf3edefe736a1a2b811bff05513d9cc20f3d7
Vinny <vinnytorde@gmail.com>
2019-04-18 16:38:17 -0500

    override

--- db/config/config.js
+++ db/config/config.js
@@ -1,8 +1,6 @@
 module.exports = {
   development: {
-    url:
-      process.env.DATABASE_URL ||
-      'postgres://cqrqickrgguxjr:2896a71ba7c0c3ee8e80e6aac6d4f822d32d82321af28b6bdbf332f42ff26a19@ec2-54-225-113-7.compute-1.amazonaws.com:5432/ddk8ulpkaar683',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: { require: true }
