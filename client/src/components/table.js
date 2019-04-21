fd938c048cf25f16c91a69df052e1e41bed7a9c4
Vinny <vinnytorde@gmail.com>
2019-04-21 14:10:53 -0500

    UI: sortable table

--- client/src/components/table.js
+++ client/src/components/table.js
@@ -1,15 +1,59 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import {
   Anchor,
+  Box,
   Table as GrommetTable,
   TableCell,
   TableBody,
   TableHeader,
-  TableRow
+  TableRow,
+  Text
 } from 'grommet'
+import { Ascend, Descend, More } from 'grommet-icons'
+
+const sortBy = field => ({
+  type: 'sort',
+  field
+})
+
+const getInitialValue = tableData => ({
+  sortedBy: null,
+  descending: null,
+  tableData
+})
+
+const reducer = (state, action) => {
+  const { field } = action
+  const { sortedBy } = state
+  switch (action.type) {
+    case 'sort': {
+      const descending = field === sortedBy ? !state.descending : true
+      let sorted
+      if (!descending) {
+        sorted = state.tableData.reverse()
+      } else {
+        sorted = state.tableData.sort((a, b) => {
+          if (a[field] < b[field]) {
+            return -1
+          }
+          if (a[field] > b[field]) {
+            return 1
+          }
+          return 0
+        })
+      }
+      return { sortedBy: field, descending, tableData: sorted }
+    }
+    default:
+      return state
+  }
+}
 
 const Table = ({ data, columnConfig, sortable }) => {
-  const [sorted, setSorted] = useState(data)
+  const [{ tableData, sortedBy, descending }, dispatch] = useReducer(
+    reducer,
+    getInitialValue(data)
+  )
 
   const row = data => {
     return (
@@ -23,38 +67,39 @@ const Table = ({ data, columnConfig, sortable }) => {
     )
   }
 
-  const sortBy = field => {
-    setSorted(
-      [...data].sort((a, b) => {
-        if (a[field] < b[field]) {
-          return -1
-        }
-        if (a[field] > b[field]) {
-          return 1
-        }
-        return 0
-      })
-    )
-  }
-
   return (
     <GrommetTable>
       <TableHeader>
         <TableRow>
-          {columnConfig.map(({ label, key }) => {
-            return (
-              <TableCell>
-                {sortable ? (
-                  <Anchor onClick={() => sortBy(key)}>{label}</Anchor>
-                ) : (
-                  { label }
-                )}
-              </TableCell>
-            )
+          {columnConfig.map(({ label, key, isSortable = true }) => {
+            let cell
+            if (sortable && isSortable) {
+              let icon
+              if (key === sortedBy) {
+                icon = descending ? <Descend /> : <Ascend />
+              } else {
+                icon = <More />
+              }
+              cell = (
+                <Anchor onClick={() => dispatch(sortBy(key))}>
+                  <Box direction="row" gap="xxsmall">
+                    {icon} {label}
+                  </Box>
+                </Anchor>
+              )
+            } else {
+              cell = (
+                <Text color="brand" weight="bold">
+                  {label}
+                </Text>
+              )
+            }
+
+            return <TableCell>{cell}</TableCell>
           })}
         </TableRow>
       </TableHeader>
-      <TableBody>{sorted.map(row)}</TableBody>
+      <TableBody>{tableData.map(row)}</TableBody>
     </GrommetTable>
   )
 }
