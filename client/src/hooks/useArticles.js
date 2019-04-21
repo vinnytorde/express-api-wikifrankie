bd184aca38052414910227e6848b65a64f6c47da
Vinny <vinnytorde@gmail.com>
2019-04-20 19:40:00 -0500

    UI: react hook refactor-articles

---
+++ client/src/hooks/useArticles.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+const useArticles = id => {
+  let endpoint = '/api/articles'
+  let initial = []
+  if (id) {
+    endpoint += '/id'
+    initial = {}
+  }
+
+  const [data, setData] = useState(initial)
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then(r => r.json())
+      .then(setData)
+  })
+
+  return data
+}
+
+export default useArticles
--- client/src/pages/articles.js
+++ client/src/pages/articles.js
@@ -1,48 +1,47 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import { Heading, Button } from 'grommet'
+import useArticles from '../hooks/useArticles'
 import Table from '../components/table'
 import { toPrettyDate } from '../utils/utils'
 
 const Articles = props => {
-  const [data, setData] = useState([])
+  const appendButtons = row => {
+    const navigateToArticle = () =>
+      props.history.push(`${props.location.pathname}/${row.id}`)
 
-  const appendButtons = data =>
-    data.map(row => {
-      const navigateToArticle = () =>
-        props.history.push(`${props.location.pathname}/${row.id}`)
+    row.actions = (
+      <Fragment>
+        <Button
+          onClick={navigateToArticle}
+          fill
+          color="neutral-3"
+          label="Modify"
+          margin="xsmall"
+        />
+        <Button
+          onClick={navigateToArticle}
+          fill
+          color="status-error"
+          label="Delete"
+          margin="xsmall"
+        />
+      </Fragment>
+    )
+    return row
+  }
 
-      row.actions = (
-        <Fragment>
-          <Button
-            onClick={navigateToArticle}
-            fill
-            color="neutral-3"
-            label="Modify"
-          />
-          <Button
-            onClick={navigateToArticle}
-            fill
-            color="status-error"
-            label="Delete"
-          />
-        </Fragment>
-      )
-      return row
-    })
-
-  useEffect(() => {
-    fetch('/api/articles')
-      .then(r => r.json())
-      .then(appendButtons)
-      .then(setData)
-  }, [])
+  const articles = useArticles()
+  let data = articles
+  if (data.length) {
+    data = articles.map(appendButtons)
+  }
 
   return (
     <div>
       <Heading margin="none" level={2}>
         All Entries
       </Heading>
-      {/* <Searchbar /> */}
+
       <Table
         data={data}
         columnConfig={[
