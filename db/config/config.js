801fb0b34caf38430c7eedc035a1976bd16be50f
Vinny <vinnytorde@gmail.com>
2019-04-21 03:38:26 -0500

    API: SQLite for dev work

--- db/config/config.js
+++ db/config/config.js
@@ -1,17 +1,7 @@
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: { require: true }
-    }
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'sqlite',
+    url: 'sqlite:../database.sqlite3'
   },
   production: {
     url: process.env.DATABASE_URL,
